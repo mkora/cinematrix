8c36d3a17e3644119c7332d1664dd604591ede53
mkor <31717889+mkora@users.noreply.github.com>
2018-08-16 14:07:17 -0700

    Change server folder structure

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const app = require('./app');
+
+http
+  .createServer(app)
+  .listen(app.get('port'), () => {
+    console.log(`Listening on http://localhost:${app.get('port')}`);
+    console.log('Press CTRL-C to stop');
+  });
