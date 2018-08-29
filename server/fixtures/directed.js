a77f00e04f866119619b7721691b8aaf9738c094
mkor <31717889+mkora@users.noreply.github.com>
2018-08-28 20:38:14 -0700

    Add fixtures data

--- server/fixtures/directed.js
+++ server/fixtures/directed.js
@@ -4,6 +4,11 @@ export default [
   { movie: 'The Godfather: Part II', person: 'Francis Ford Coppola' },
   { movie: 'The Dark Knight', person: 'Christopher Nolan' },
   { movie: '12 Angry Men', person: 'Sidney Lumet' },
+  { movie: 'Schindler\'s List', person: 'Steven Spielberg' },
+  { movie: 'The Lord of the Rings: The Return of the King', person: 'Peter Jackson' },
+  { movie: 'Pulp Fiction', person: 'Quentin Tarantino' },
+  { movie: 'The Good, the Bad and the Ugly', person: 'Sergio Leone' },
+  { movie: 'Fight Club', person: 'David Fincher' },
 ];
 
 //  { movie: '', person: '' },
