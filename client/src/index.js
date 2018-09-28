9f5ff394582b7ea28503675a5bc8881aa8e60045
mkor <31717889+mkora@users.noreply.github.com>
2018-09-27 18:32:48 -0700

    Revert deleting regesterServiceWorker

--- client/src/index.js
+++ client/src/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './containers/App';
+import registerServiceWorker from './utils/registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
