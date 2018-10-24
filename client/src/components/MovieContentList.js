696f92f9216dc0d07c18b2dd6cce968da8c88a0e
mkor <31717889+mkora@users.noreply.github.com>
2018-10-24 18:05:57 +0700

    Add movies api call to movie component

--- client/src/components/MovieContentList.js
+++ client/src/components/MovieContentList.js
@@ -1,38 +1,71 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ContentList from './ContentList';
+import { movies } from  '../api';
 
 class MovieContentList extends Component {
   state = {
-    data: [
-      {
-        summary: 'Movie title 1, year', // Actor full name
-        mainDetails: 'eq pic || duration & number of episodes #1,country', // Actor pic || additial info
-        secondaryDetails: 'Movie synopsis 1', // Actor bio
-        editAction: 'movie-edit/1',
-        deleteAction: 'movie-edit/1',
-      },
-      {
-        summary: 'Movie title 2, year', // Actor full name
-        mainDetails: 'eq pic || duration & number of episodes #2, country', // Actor pic || additial info
-        secondaryDetails: 'Movie synopsis 2', // Actor bio
-        editAction: 'movie-edit/2',
-        deleteAction: 'movie-edit/2',
-      },
-      {
-        summary: 'Movie title 3, year', // Actor full name
-        mainDetails: 'eq pic || duration & number of episodes #3, country', // Actor pic || additial info
-        secondaryDetails: 'Movie synopsis 3', // Actor bio
-        editAction: 'movie-edit/3',
-        deleteAction: 'movie-edit/3',
-      },
-      ],
+    data: [],
+    isError: false, // SnackBars with messages -> but I don't want it here
+    isLoading: false, // CircularProgress -> but I don't want it here
   };
+
+  async componentDidMount() {
+    try {
+      const data = await movies();
+      if(data.success) {
+        /**
+         * TODO: parse data as:
+            {
+              summary: 'Movie title 1, year', // Actor full name
+              mainDetails: 'eq pic || duration & number of episodes #1,country', // Actor pic || additial info
+              secondaryDetails: 'Movie synopsis 1', // Actor bio
+              editAction: 'movie-edit/1',
+              deleteAction: 'movie-edit/1',
+            },
+         *
+         *
+         */
+        this.setState({
+          data,
+          isError: false,
+          isLoading: false,
+        });
+      } else {
+        this.setState({
+          isError: true,
+          isLoading: false,
+        });
+        console.log(data.error);
+      }
+    } catch (err) {
+      this.setState({
+        isError: true,
+        isLoading: false,
+      });
+      console.log(err);
+    }
+  }
 
   render() {
     const {
       data,
+      isLoading,
+      isError
     }  = this.state;
+
+    if (isLoading) {
+      return (
+        <div>LOADING...</div>
+      );
+    }
+
+    if (isError) {
+      return (
+        <div>ERROR... Check out console.log</div>
+      );
+    }
+
     return (
       <ContentList data={data} />
     );
